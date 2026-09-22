9490dd9084b1a22e44c49833c34f91e9a5edcfab
shaik shabbir <shabbir.shaikbp@gmail.com>
2026-09-22 22:36:39 +0530

    updated backend

--- 04_TF_State/backend.tf
+++ 04_TF_State/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name   = "terraform-rg"
-    storage_account_name  = "terraformrg"
+    resource_group_name   = "storagetfstate011"
+    storage_account_name  = "kml_rg_main-52ab83f918254310"
     container_name        = "tfstate"
     key                   = "terraform.tfstate"
   }
--- 06_vm_linux/backend.tf
+++ 06_vm_linux/backend.tf
@@ -1,5 +1,12 @@
 # Values come from the Azure DevOps pipeline (-backend-config).
 # Create the storage account once. See 06_vm_linux/PIPELINE.md
 terraform {
-  backend "azurerm" {}
+  backend "azurerm" {
+    resource_group_name   = "storagetfstate011"
+    storage_account_name  = "kml_rg_main-52ab83f918254310"
+    container_name        = "tfstate"
+    key                   = "terraform.tfstate"
+  }
 }
+
+
